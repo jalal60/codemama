773cef4d53c2dec830801cd32a9496743d409cac
Md. Shahjalal <jalal2k4@gmail.com>
2023-10-18 13:05:29 +0600

    Update lookfor_10.php

--- lookfor_10.php
+++ lookfor_10.php
@@ -1,12 +1,24 @@
 <?php
-   	function lookforTen(int $a,int $b){
-	  if($a==10||$b==10||$a+$b==10){
-	    echo "True";
-	  }
-	  else{
-	    echo "False";
-	  }
-	}
-  fscanf(STDIN, "%d %d\n",$a,$b);
-	lookforTen($a,$b);
+/*
+Problem Statement
+Write a program to create a function that takes two arguments. 
+Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
+
+Input
+The input consists of two integers:
+5 5
+Output
+The output will print "True" if one of the input is 10 or their sum is 10. Otherwise, it will print "False":
+True
+*/
+  function lookforTen(int $a,int $b){
+  if($a==10||$b==10||$a+$b==10){
+    echo "True";
+  }
+  else{
+    echo "False";
+  }
+}
+fscanf(STDIN, "%d %d\n",$a,$b);
+lookforTen($a,$b);
 ?>
