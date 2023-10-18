3ee1739b046857a123ef4fb07b07c85a9cb0e9cc
Md. Shahjalal <jalal2k4@gmail.com>
2023-10-18 13:03:47 +0600

    Update concate_string.php

--- concate_string.php
+++ concate_string.php
@@ -4,10 +4,12 @@ Concatenate Two String
 Problem Statement: Write a program where two strings will be given, firstName and lastName, return a single string in the format "lastName, firstName".
 
 Input
-The input consists of two strings.
+The input consists of two strings:
+John Doe
 
 Output
-The output will print a string.
+The output will print a string:
+Doe, John
 */
   fscanf(STDIN, "%s %s", $firstName,$lastName );
   echo $lastName.", ".$firstName;
